47e6922f57d9775e348798cfc65eb829d843c1f4
kultosh <kulgole90@gmail.com>
2025-05-16 17:43:29 +0545

    Added Frontend layout and also added header, footer and breadcrumb component

---
+++ frontend/src/components/frontend/FrontendBreadcrumb.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="bg-secondary text-white py-4 mb-4">
+        <div class="container">
+            <div class="row">
+            <div class="col-md-12">
+                <h1 class="mb-2">{{ title }}</h1>
+                <ol class="breadcrumb mb-0">
+                    <li v-for="(item, index) in breadcrumb" :key="index" class="breadcrumb-item">
+                        <router-link :to="item.path">{{ item.name }}</router-link>
+                    </li>
+                    <li class="breadcrumb-item active">{{ title }}</li>
+                </ol>
+            </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    breadcrumb: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ frontend/src/layouts/FrontendLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex flex-column min-vh-100">
+    <FrontendHeader />
+    <main class="flex-fill my-3 container">
+      <router-view />
+    </main>
+    <FrontendFooter />
+  </div>
+</template>
+
+<script>
+import FrontendHeader from "@/components/frontend/FrontendHeader.vue";
+import FrontendFooter from "@/components/frontend/FrontendFooter.vue";
+
+export default {
+  components: {
+    FrontendHeader,
+    FrontendFooter,
+  },
+};
+</script>
+
+<style scoped>
+main {
+  padding: 20px;
+}
+</style>
