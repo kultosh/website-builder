ede228a9d2118dba0c88b8aa821da360c41edff1
kultosh <kulgole90@gmail.com>
2025-05-18 21:06:21 +0545

    Add Slider CRUD API Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\PageController;
+use App\Http\Controllers\Admin\SliderController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Frontend\PageController as FrontendPageController;
 use Illuminate\Support\Facades\Route;
@@ -20,6 +21,15 @@ Route::middleware('auth:sanctum')->group(function () {
 
     });
     Route::get('/parent/pages', [PageController::class, 'parentPages']);
+
+    Route::prefix('sliders')->controller(SliderController::class)->group(function () {
+        Route::get('/', 'index');
+        Route::post('/', 'store');
+        Route::get('/{id}', 'edit');
+        Route::put('/{id}', 'update');
+        Route::delete('/{id}', 'destroy');
+
+    });
 });
 
 Route::get('/home', [FrontendPageController::class, 'getHomePageSections']);
