7fa3e739cccf409a16101aa0ed3b9422600cf5ae
kultosh <kulgole90@gmail.com>
2025-05-19 00:58:50 +0545

    Display slider into the home page from database

--- frontend/src/views/frotnend/HomePage.vue
+++ frontend/src/views/frotnend/HomePage.vue
@@ -1,39 +1,7 @@
 <template>
   <div>
     <!-- Hero / Slider Section -->
-    <!-- <section class="hero-section mb-5">
-      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="https://via.placeholder.com/700x260?text=Slide+2" class="d-block w-100" alt="Slide 1">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Slider URL</h5>
-                        <p><button type="button" class="btn btn-outline-info">Redirect</button></p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="https://via.placeholder.com/700x260?text=Slide+1" class="d-block w-100" alt="Slide 2">
-                </div>
-                    <div class="carousel-item">
-                <img src="https://via.placeholder.com/700x260?text=Slide+3" class="d-block w-100" alt="Slide 3">
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
-        </div>
-    </section> -->
-
+    <SliderSection :sliderList="sliderList" />
     <!-- Single Page Sections -->
     <HomePageSectionSingle v-if="singlePageList.length" :singlePageList="singlePageList" />
     <!-- Parent Page Sections -->
@@ -45,17 +13,20 @@
 import { getHomePageSectionData } from '@/services/frontendApi';
 import HomePageSectionSingle from "@/components/frontend/sections/HomePageSectionSingle.vue";
 import HomePageSectionParent from "@/components/frontend/sections/HomePageSectionParent.vue";
+import SliderSection from "@/components/frontend/sections/SliderSection.vue";
 
 export default{
     props: ['pageId'],
     components: {
         HomePageSectionSingle,
         HomePageSectionParent,
+        SliderSection
     },
     data() {
         return {
             singlePageList: [],
             parentChildPageList: [],
+            sliderList: [],
         }
     },
     beforeMount() {
@@ -68,9 +39,7 @@ export default{
                 const data = response.data.content;
                 this.singlePageList = data.single;
                 this.parentChildPageList = data.parent;
-
-                console.log('singlePageList>>', this.singlePageList);
-                console.log('parentChildPageList>>', this.parentChildPageList);
+                this.sliderList = data.sliders;
             })
             .catch(err => {
                 console.error('Page not found', err);
