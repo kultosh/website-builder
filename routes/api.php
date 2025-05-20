4176829e79a9f02ab311ee2b41c26c4884f57ed1
kultosh <kulgole90@gmail.com>
2025-05-20 20:16:58 +0545

    Add migration seeder routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\SliderController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Frontend\PageController as FrontendPageController;
 use App\Http\Controllers\SettingController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
@@ -42,3 +43,15 @@ Route::get('/home', [FrontendPageController::class, 'getHomePageSections']);
 Route::get('/menu/pages', [FrontendPageController::class, 'getMenuPages']);
 Route::get('/parent/{id}/childs', [FrontendPageController::class, 'getChildPages']);
 Route::get('/{slug}', [FrontendPageController::class, 'showPage'])->where('slug', '.*');
+
+// FOR DEPLOYMENT DUE TO NO AVAILIBILITY OF SHELL IN TEST SERVER, TO BE REMOVE ONCE DONE
+Route::prefix('deployment')->group(function () {
+    Route::get('/migrate', function () {
+        Artisan::call('migrate', ['--force' => true]);
+        return 'Database migrations completed.';
+    });
+    Route::get('/seed', function () {
+        Artisan::call('db:seed', ['--force' => true]);
+        return 'Database seeding completed.';
+    });
+});
